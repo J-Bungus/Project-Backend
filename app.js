90e4ec159645edf936d42eb593e60dd263377508
Justin Wang <justin.wang1ab@gmail.com>
2024-03-02 13:49:33 -0500

    add cors

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const routes = require('./routes');
+const cons = require('cors');
+
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+    origin: '*',
+}));
+
 app.use('/', routes);
 
 const port = process.env.PORT || 8080;
